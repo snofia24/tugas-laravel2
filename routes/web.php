6be76356f21fe56ee4fd87db8f44f715ff9fd249
sucinofia009 <sucinofia009@gmail.com>
2023-08-29 17:35:35 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PerpustakaanController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/buku', function () {
+    return view('welcome');
+});
+
+Route::get('/buku', [PerpustakaanController::class,'buku'])
+->name('get_buku');
+
+Route::get('/petugas', [PerpustakaanController::class,'petugas'])
+->name('get_petugas');
+
+Route::get('/anggota', [PerpustakaanController::class,'anggota'])
+->name('get_anggota');
